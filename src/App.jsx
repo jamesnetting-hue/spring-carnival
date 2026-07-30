4c731f876fae1f76af1828b4639acfd17b03b279
jamesnetting-hue <james.netting@gmail.com>
2026-07-30 11:25:11 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1734,10 +1734,6 @@ function LobbyScreen({races,bets,account,leaderboard,getRaceBalance,onSelect,sea
             })}
             </div>
 
-            </div>
-
-            </div>
-
 
           </div>
           );
